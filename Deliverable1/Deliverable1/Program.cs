be46b6ff0772885606f543285d0d8c12ed8b4e0f
Jered O'Grady <jered9715@gmail.com>
2024-01-11 15:04:38 -0500

    first commit

---
+++ Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,103 @@
+﻿//Deliverable 1 for C# BootCamp
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //Declaring variables for Restock Program
+        //Initial variable values established as base line inventory
+        int sodaStock = 100;
+        int restockSoda = 40;
+
+        int chipsStock = 40;
+        int restockChips = 20;
+
+        int candyStock = 60;
+        int restockCandy = 40;
+
+        //Intro string for restock program
+        Console.WriteLine("Welcome to the restocking tool!");
+        Console.WriteLine();
+
+        //Gathering input for Soda Stock adjustment
+        Console.WriteLine("How many Sodas have been sold today? " + sodaStock + " are in stock.");
+        Console.Write(">");
+        sodaStock = int.Parse(Console.ReadLine());
+
+
+        //If statement used to determine acceptable user input for Soda
+        if (sodaStock <= 100)
+        {
+            sodaStock = 100 - sodaStock;
+            Console.WriteLine("There are " + sodaStock + " Sodas left");
+        }
+        else
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted");
+        }
+
+        Console.WriteLine();
+
+
+        //Gathering input for Chips stock adjustment
+        Console.WriteLine("How many Chips have been sold today? " + chipsStock + " are in stock.");
+        Console.Write(">");
+        chipsStock = int.Parse(Console.ReadLine());
+
+
+        //If statement used to determine acceptable user input for Chips
+        if (chipsStock <= 40)
+        {
+            chipsStock = 40 - chipsStock;
+            Console.WriteLine("There are " + chipsStock + " Chips left");
+        }
+        else
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted");
+        }
+        Console.WriteLine();
+
+        //Gathering input for Candy Stock
+        Console.WriteLine("How many Candy have been sold today? " + candyStock + " are in stock.");
+        Console.Write(">");
+        candyStock = int.Parse(Console.ReadLine());
+
+
+        //If statement used to determine acceptable user input for Candy
+        if (candyStock <= 60)
+        {
+            candyStock = 60 - candyStock;
+            Console.WriteLine("There are " + candyStock + " Candy left");
+        }
+        else
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        //list of items that have exceeded the minimum value for restock
+        Console.WriteLine("Thank you for filling out the values. Here's what needs to be restocked.");
+
+
+        //if statement for Soda restock message
+        if (sodaStock <= restockSoda)
+            Console.WriteLine("Soda needs to be restocked");
+
+        //if statement for Chips restock message
+        if (chipsStock <= restockChips)
+            Console.WriteLine("Chips needs to be restocked");
+
+        //if statement for Candy restock message
+        if (candyStock <= restockCandy)
+            Console.WriteLine("Candy needs to be restocked");
+
+        Console.WriteLine();
+
+
+        //ending statement
+        Console.WriteLine("Goodbye!");
+    }
+}
---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,114 @@
+﻿//Deliverable 2 Coin FLip Challenge
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //Establishing user variables
+        string userName = string.Empty;
+        string userAnswer = string.Empty;
+        string challengeAnswer = string.Empty;
+        int score = 0;
+
+        //Welcome greeting and request for username
+        Console.WriteLine("Welcome to the COIN FLIP CHALLENGE!");
+        Console.WriteLine("What is your name?");
+        Console.Write(">");
+        userName = Console.ReadLine();
+
+        //added for spacing in console
+        Console.WriteLine();
+
+        //Proposition of challenge
+
+        Console.WriteLine("Welcome " + userName + ". Do you want to do the COIN FLIP CHALLENGE? Yes/No");
+        Console.Write(">");
+        challengeAnswer = Console.ReadLine();
+        challengeAnswer = challengeAnswer.ToLower();
+
+        //added for spacing in console
+        Console.WriteLine();
+
+        //Beginning If Else for acceptance or decline of challenge
+        if (challengeAnswer == "yes")
+        {
+            //creating loop to allow for Rounds
+            for (int round = 0; round < 5; round++)
+            {
+                //this is to make the randomized flip for comparing
+                Random rnd = new Random();
+                int coin = rnd.Next(0, 2);
+
+                //determining value of 0 and 1
+
+
+                //ask user for answer to coin flip
+                Console.WriteLine("Heads or Tails?");
+                Console.Write(">");
+                userAnswer = Console.ReadLine();
+                userAnswer = userAnswer.ToLower();
+
+                //comparison of user answer of heads or tail to coin flip value
+                //asign score based on correct answer
+                //Heads = 1
+                //Tails = 0
+                if (userAnswer == "heads")
+                {
+                    if (coin == 1)
+                    {
+                        Console.WriteLine("Correct!");
+                        score++;
+
+                        //added for spacing in console
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong!");
+
+                        //added for spacing in console
+                        Console.WriteLine();
+                    }
+                }
+                else if (userAnswer == "tails")
+                {
+                    if (coin == 0)
+                    {
+                        Console.WriteLine("Correct!");
+                        score++;
+
+                        //added for spacing in console
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong!");
+
+                        //added for spacing in console
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Answer not recognized, no points given :( ");
+                    //added for spacing in console
+                    Console.WriteLine();
+                }
+
+
+
+            }
+
+            // End of game summary
+            Console.WriteLine("Thank you " + userName + ", you got a score of " + score + "!");
+        }
+        else if (challengeAnswer == "no")
+        {
+            Console.WriteLine("You are a coward " + userName);
+        }
+        else
+        {
+            Console.WriteLine("Answer not recognized, please restart the game.");
+        }
+    }
+}
